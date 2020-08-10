77940aa15cda9c9bd4b8122f4d0a90cd8c20fd7e
Ruben Amador <ruben.amador@live.u-tad.com>
2020-08-10 18:18:29 +0200

    Ejemplo de intercambio de mensajes entre actores Akka agregado

--- akka-quickstart-scala/src/main/scala/com/example/AkkaQuickstart.scala
+++ akka-quickstart-scala/src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,80 +1,48 @@
-//#full-example
-package com.example
+import akka.actor._
 
+case object PingMessage
+case object PongMessage
+case object StartMessage
+case object StopMessage
 
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
-import com.example.GreeterMain.SayHello
-
-//#greeter-actor
-object Greeter {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
-
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    //#greeter-send-messages
-    message.replyTo ! Greeted(message.whom, context.self)
-    //#greeter-send-messages
-    Behaviors.same
-  }
-}
-//#greeter-actor
-
-//#greeter-bot
-object GreeterBot {
-
-  def apply(max: Int): Behavior[Greeter.Greeted] = {
-    bot(0, max)
-  }
-
-  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
-    Behaviors.receive { (context, message) =>
-      val n = greetingCounter + 1
-      context.log.info("Greeting {} for {}", n, message.whom)
-      if (n == max) {
-        Behaviors.stopped
-      } else {
-        message.from ! Greeter.Greet(message.whom, context.self)
-        bot(n, max)
-      }
+class Ping(pong: ActorRef) extends Actor {
+    var count = 0
+    def incrementAndPrint { count += 1; println("ping") }
+    def receive = {
+        case StartMessage =>
+            incrementAndPrint
+            pong ! PingMessage
+        case PongMessage =>
+            incrementAndPrint
+            if (count > 99) {
+                sender ! StopMessage
+                println("ping stopped")
+                context.stop(self)
+            } else {
+                sender ! PingMessage
+            }
+        case _ => println("Ping got something unexpected.")
     }
 }
-//#greeter-bot
 
-//#greeter-main
-object GreeterMain {
-
-  final case class SayHello(name: String)
-
-  def apply(): Behavior[SayHello] =
-    Behaviors.setup { context =>
-      //#create-actors
-      val greeter = context.spawn(Greeter(), "greeter")
-      //#create-actors
-
-      Behaviors.receiveMessage { message =>
-        //#create-actors
-        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
-        //#create-actors
-        greeter ! Greeter.Greet(message.name, replyTo)
-        Behaviors.same
-      }
+class Pong extends Actor {
+    def receive = {
+      case PingMessage =>
+          println(" pong")
+          sender ! PongMessage
+      case StopMessage =>
+          println("pong stopped")
+          context.stop(self)
+      case _ => println("Pong got something unexpected.")
     }
 }
-//#greeter-main
 
-//#main-class
-object AkkaQuickstart extends App {
-  //#actor-system
-  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "AkkaQuickStart")
-  //#actor-system
-
-  //#main-send-messages
-  greeterMain ! SayHello("Charles")
-  //#main-send-messages
+object PingPongTest extends App {
+    val system = ActorSystem("PingPongSystem")
+    val pong = system.actorOf(Props[Pong], name = "pong")
+    val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
+    // start the action
+    ping ! StartMessage
+    // commented-out so you can see all the output
+    //system.shutdown
 }
-//#main-class
-//#full-example
