463f6a1071d2a1d9ea95c7f09c2824df6e5b984c
Ruben Amador <ruben.amador@live.u-tad.com>
2020-10-27 19:35:05 +0100

    Codigo inicial de Zookeeper Connection modificado para crear znodes, obtener su contenido, modificarlo y borrar znodes

--- zookeeper-quickstart-scala/build.sbt
+++ zookeeper-quickstart-scala/build.sbt
@@ -7,15 +7,10 @@ organization := "com.example"
 val projectScalaVersion = "2.12.1"
 scalaVersion := "2.12.1"
 
-lazy val SparkVersion = "2.4.4"
-lazy val ZookeeperVersion = "3.6.2"
+lazy val ZookeeperVersion = "3.6.1"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % SparkVersion,
-  "org.apache.spark" %% "spark-sql" % SparkVersion,
-  "org.apache.zookeeper" % "zookeeper" % ZookeeperVersion,
-  "ch.qos.logback" % "logback-classic" % "1.2.3",
-  "org.scalatest" %% "scalatest" % "3.1.0" % Test
+  "org.apache.zookeeper" % "zookeeper" % ZookeeperVersion
 )
 
 scalaVersion in ThisBuild := projectScalaVersion
--- zookeeper-quickstart-scala/src/main/scala/com/example/ZookeeperNodes.scala
+++ zookeeper-quickstart-scala/src/main/scala/com/example/ZookeeperNodes.scala
@@ -1,12 +1,5 @@
 package com.example
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Dataset, SparkSession}
-import org.apache.spark.sql.functions._
-import org.apache.log4j.{Level, Logger}
-
-import scala.io.StdIn.readLine
-
 import org.apache.zookeeper.ZooKeeper
 import org.apache.zookeeper.KeeperException
 import org.apache.zookeeper.WatchedEvent
@@ -18,7 +11,8 @@ import org.apache.zookeeper.data.Stat
 import org.apache.zookeeper.CreateMode
 import org.apache.zookeeper.ZooDefs
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CountDownLatch
+import scala.io.StdIn.readLine
 
 class ZooKeeperConnection {
     var zoo: ZooKeeper = _
@@ -26,14 +20,14 @@ class ZooKeeperConnection {
 
     // Method to connect zookeeper ensemble
     def connect(host: String) : ZooKeeper = {    
-        var zoo = new ZooKeeper(host, 5000, new Watcher() {        
+        var zoo = new ZooKeeper(host, 2181, new Watcher() { //in my case ZK uses port 2181 --- you can put another port
             def process(we: WatchedEvent) = {
                 if (we.getState() == KeeperState.SyncConnected) {
                     connectedSignal.countDown()
                 }
             }        
         })
-        //connectedSignal.await()
+        connectedSignal.await()
         println("Connecting Zookeeper ...")
         return zoo
     }
@@ -46,15 +40,20 @@ class ZooKeeperConnection {
 
 object ZookeeperServerTest extends App {
     println("Beginning...")
+    
+    // Creating Zookeeper connection
     val conn = new ZooKeeperConnection()
-    val zk = conn.connect("localhost")
+    val zk = conn.connect("192.168.99.100") //this host is my docker-machine ip --- you can put another host as "localhost"
     println("Creating Zookeeper Connection...")
     
-    val path = "/MyFirstZnode"
-    val data = "My first zookeeper app".getBytes()
+    // Creating path for znode
+    var path = "/MyFirstZnode"
+    var data = "My first zookeeper app"
+    println(zk)
     println("Creating ZKPath...")
     
-    val stat : Stat = zk.exists(path, true)
+    // Checking if znode exists
+    var stat : Stat = zk.exists(path, true)
     println("Checking ZNode exists...")
     
     if (stat != null) {
@@ -62,9 +61,98 @@ object ZookeeperServerTest extends App {
     }
     else {
         println("Node does not exists")
-        zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+        zk.create(path, data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT) // Creating znode
         println("Creating ZNode in one path...")
     }
+    
+    // Input for interaction with Zookeeper Server
+    var option = "0"
+    while (option != "6" && option.toLowerCase != "exit") {
+        print("OPTIONS: \n 1. Create ZNode \n 2. Check ZNode \n 3. Get Data \n 4. Set Data \n 5. Delete ZNode \n 6. Exit \n")
+        println("Enter your option for Zookeeper: ")
+        option = readLine()
+        
+        if (option == "1" || option.toLowerCase == "create znode") { // Create ZNode
+            println("Enter ZKPath: ")
+            path = readLine()
+            stat = zk.exists('/' + path, true)
+            
+            if (stat != null) {
+                println("Node already exists: /" + path)
+            }
+            else {
+                println("Enter data for ZKPath: ")
+                data = readLine()
+                zk.create('/' + path, data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+                println("Creating ZNode in one path...")
+            }
+            Thread.sleep(2000) // wait for 2 seconds
+        }
+        else if (option == "2" || option == "check znode") { // Check ZNode
+            println("Enter ZKPath: ")
+            path = readLine()
+            stat = zk.exists('/' + path, true)
+            println("Checking ZNode exists...")
+    
+            if (stat != null) {
+                println("Node exists and the node version is " + stat.getVersion())
+            }
+            else {
+                println("Node does not exists")
+            }
+            Thread.sleep(3000) // wait for 3 seconds
+        }
+        else if (option == "3" || option.toLowerCase == "get data") { // Get Data
+            println("Enter ZKPath: ")
+            path = readLine()
+            try { 
+                data = new String(zk.getData('/' + path, false, null), "UTF-8")
+                println("Getting ZNode data...")
+                println(data)
+            }
+            catch {
+                case e: KeeperException => println("Node does not exists")
+            }
+            Thread.sleep(4000) // wait for 4 seconds
+        }
+        else if (option == "4" || option.toLowerCase == "set data") { // Set Data
+            println("Enter ZKPath: ")
+            path = readLine()
+            stat = zk.exists('/' + path, true)
+            try {
+                println("Enter new data for ZKPath: ")
+                data = readLine()                
+                zk.setData('/' + path, data.getBytes(), stat.getVersion())
+                println("Setting ZNode data...")
+            }
+            catch {
+                case e: KeeperException => println("Node does not exists")
+            }
+            Thread.sleep(4000) // wait for 4 seconds
+        }
+        else if (option == "5" || option.toLowerCase == "delete znode") { // Delete ZNode
+            println("Enter ZKPath: ")
+            path = readLine()
+            stat = zk.exists('/' + path, true)
+            
+            if (stat != null) {
+                zk.delete('/' + path, stat.getVersion())
+                println("Deleting ZNode in one path...")
+            }
+            else {
+                println("Node does not exists")
+            }
+            Thread.sleep(2000) // wait for 2 seconds
+        }
+        else {
+            if (option != "6" && option.toLowerCase != "exit") {
+                println("Option does not exists")
+            }
+            Thread.sleep(2000) // wait for 2 seconds
+            
+        }
+    }
 
-    conn.close()
+    zk.close()
+    println("Closing Zookeeper Connection...")
 }
